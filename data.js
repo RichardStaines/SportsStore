f93c7ef6c25da25d611501edc59d489b08407f2c
Richard Staines <richard.staines@live.com>
2019-10-23 21:55:32 +0100

    Add project files and REST test data file

---
+++ data.js
@@ -0,0 +1,16 @@
+module.exports = function () {
+  return {
+    products: [
+      { id: 1, name: "Kayak", category: "Watersports", description: "A small boat", price: 315},
+      { id: 2, name: "Lifejacket", category: "Watersports", description: "A floatation device", price: 55},
+      { id: 3, name: "Football", category: "Football", description: "UEFA official ball", price: 37.99},
+      { id: 4, name: "Corner Flags", category: "Football", description: "UEFA approved", price: 77.78},
+      { id: 5, name: "Stadium", category: "Football", description: "flat packed", price: 1565000},
+      { id: 6, name: "Cloth Cap", category: "Fashion", description: "Traditional design", price: 43.99},
+      { id: 7, name: "Shirt", category: "Fashion", description: "Black", price: 20},
+      { id: 8, name: "Shorts", category: "Fashion", description: "Khaki", price: 25},
+      { id: 9, name: "Socks", category: "Fashion", description: "Yellow", price: 3.99}
+    ],
+    orders: []
+  }
+}
