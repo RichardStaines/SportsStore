c1b625f9f15ca783e1c2622099c09f892c9b7497
Richard Staines <richard.staines@live.com>
2019-10-23 22:46:52 +0100

    Add Password Authentication.

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -6,11 +6,13 @@ import { Cart } from "./cart.model";
 import { Order } from "./order.model";
 import { OrderRepository } from "./order.repository";
 import { RestDataSource } from "./rest.datasource";
+import { AuthService } from "./auth.service";
 
 @NgModule({
   imports: [HttpClientModule],
   providers: [ProductRepository, Cart, Order, OrderRepository,
-    { provide: StaticDataSource, useClass: RestDataSource }
+              AuthService, RestDataSource,
+    { provide: StaticDataSource, useClass: RestDataSource}
   ]
 })
 export class ModelModule { }
